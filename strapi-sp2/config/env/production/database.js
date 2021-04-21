f382b58c5685a2b4ef0b98dbd2617f011fcac164
Glenn Key <glennkey78@live.no>
2021-04-22 00:03:18 +0200

    made updated changes to database.js

--- strapi-sp2/config/env/production/database.js
+++ strapi-sp2/config/env/production/database.js
@@ -2,24 +2,25 @@ const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-        ssl: {
-          rejectUnauthorized: false,
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'postgres',
+          host: env('DATABASE_HOST', 'localhost'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'strapi'),
+          username: env('DATABASE_USERNAME', 'strapi'),
+          password: env('DATABASE_PASSWORD', 'strapi'),
+          schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+          ssl: {
+            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+          },
+        },
+        options: {
+          ssl: env.bool('DATABASE_SSL', false),
         },
       },
-      options: {
-        ssl: true,
-      },
     },
-  },
-});
+  });
